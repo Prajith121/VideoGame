c48691775517dbfb8138916bbd3d21bb2e2908e7
Prajith121 <147348731+Prajith121@users.noreply.github.com>
2024-10-12 18:14:44 -0700

    Added Docs cleaned up stuff increased move speed

--- Player/CharacterBody2D.gd
+++ Player/CharacterBody2D.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 200.0
 const SPRINTSPEED = 400.0
 var SPRINTTIME = 200
 var CANSPRINT = true
-var mouse_left_down: bool = false
+
 func _physics_process(delta):
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	var mouse_left_down: bool = Input.is_action_pressed("SPRINT")
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	if mouse_left_down:
@@ -27,20 +29,13 @@ func move(x,y,SPEED):
 	v.y = y
 	return (v.normalized() * SPEED)
 
-func _input( event ):
-	if event is InputEventMouseButton:
-		if event.button_index == 1 and event.is_pressed():
-			mouse_left_down = true
-		elif event.button_index == 1 and not event.is_pressed():
-			mouse_left_down = false
-
 
 func _on_sprint_timer_timeout():
 	CANSPRINT = false
-	$Timer.start(-1)
+	$SprintStopTimer.start(-1)
 	$SprintTimer.stop()
 
-func _on_timer_timeout():
+func _on_sprint_stop_timer_timeout():
 	CANSPRINT = true 
 	$SprintTimer.start(-1)
-	$Timer.stop()
+	$SprintStopTimer.stop()
