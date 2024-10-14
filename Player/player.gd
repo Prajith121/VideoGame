bca030edf6a249897cf6e33f7535e4a169c71f5f
Prajith121 <147348731+Prajith121@users.noreply.github.com>
2024-10-13 23:30:59 -0700

    Animations Added + Starting inventory

--- Player/player.gd
+++ Player/player.gd
@@ -5,7 +5,10 @@ const SPEED = 200.0
 const SPRINTSPEED = 400.0
 var SPRINTTIME = 200
 var CANSPRINT = true
-@onready var stamina = get_parent().get_node("Stamina")
+@onready var stamina = get_node("Stamina")
+@export var inv : Inv
+var lastMoveDir = "right"
+var idle: bool = true
 func _ready():
 	stamina.value = 100
 func _physics_process(_delta):
@@ -28,6 +31,7 @@ func _physics_process(_delta):
 			if stamina.value != 100:
 				stamina.value += 0.08
 			velocity = move(direction_x,direction_y,SPEED)
+			
 	else:
 		if stamina.value != 100:
 			stamina.value += 0.08
@@ -35,7 +39,35 @@ func _physics_process(_delta):
 	move_and_slide()
 	
 func move(x,y,SPEED):
+	$Player_Animations.flip_h = false
 	var v : Vector2
 	v.x = x
 	v.y = y
+	if x>0 and y == 0:
+		lastMoveDir = "right"
+		$Player_Animations.play("walk_right")
+	elif x<0 and y == 0:
+		lastMoveDir = "left"
+		$Player_Animations.flip_h = true
+		$Player_Animations.play("walk_right")
+		
+	if y>0:
+		lastMoveDir = "down"
+		$Player_Animations.play("walk_down")
+	elif y<0:
+		lastMoveDir = "up"
+		$Player_Animations.play("walk_up")
+	if x== 0 and y == 0:
+		if lastMoveDir == "right":
+			$Player_Animations.play("idle_right")
+		elif lastMoveDir == "up":
+			$Player_Animations.play("idle_up")
+		elif lastMoveDir == "down":
+			$Player_Animations.play("idle_down")
+		elif lastMoveDir == "left":
+			$Player_Animations.flip_h = true
+			$Player_Animations.play("idle_right")
+			
+	
 	return (v.normalized() * SPEED)
+			
