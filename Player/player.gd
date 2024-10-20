79533a3ee91c11992c3261f8d0c2d809a7ed34d3
Prajith121 <147348731+Prajith121@users.noreply.github.com>
2024-10-19 20:35:49 -0700

    Just Reworked Stamina to be future proof

--- Player/player.gd
+++ Player/player.gd
@@ -5,36 +5,41 @@ const SPEED = 200.0
 const SPRINTSPEED = 400.0
 var SPRINTTIME = 200
 var CANSPRINT = true
-@onready var stamina = get_node("Stamina")
+var stamina = 100
 @export var inv : Inv
+signal staminaSignal(stamina)
 var lastMoveDir = "right"
 var idle: bool = true
 func _ready():
-	stamina.value = 100
+	stamina = 100
+	emit_signal("staminaSignal",stamina)
 func _physics_process(_delta):
-	if stamina.value > 0:
+	if stamina > 0:
 		CANSPRINT = true
-	
+		
 	else: 
 		CANSPRINT = false
 	var mouse_left_down: bool = Input.is_action_pressed("SPRINT")
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	if mouse_left_down:
-		if CANSPRINT:
-			if stamina.value < -0.2:
-				stamina.value = -10
+		if CANSPRINT and (direction_x != 0 or direction_y != 0):
+			if stamina< -0.2:
+				stamina = -10
 			else:
-				stamina.value -= 0.1
+				stamina -= 0.1
+				emit_signal("staminaSignal",stamina)
 			velocity = move(direction_x,direction_y,SPRINTSPEED)
 		else:
-			if stamina.value != 100:
-				stamina.value += 0.08
+			if stamina != 100:
+				stamina += 0.08
+				emit_signal("staminaSignal",stamina)
 			velocity = move(direction_x,direction_y,SPEED)
 			
 	else:
-		if stamina.value != 100:
-			stamina.value += 0.08
+		if stamina != 100:
+			stamina += 0.08
+			emit_signal("staminaSignal",stamina)
 		velocity = move(direction_x,direction_y,SPEED)
 	move_and_slide()
 	
